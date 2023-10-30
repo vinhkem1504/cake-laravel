b448304af1a8464c3db5a77c0e0d451645628be4
trmylinh <linhninh2@gmail.com>
2023-10-30 15:14:18 +0700

    add model

---
+++ app/Models/Bill_details.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bill_details extends Model
+{
+    use HasFactory;
+    protected $table = "Bill_details";
+}
