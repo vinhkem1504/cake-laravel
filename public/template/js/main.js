1a279eab6a8cc9f159665746b8d5fda9b30d86ff
trmylinh <linhninh2@gmail.com>
2023-11-06 19:00:45 +0700

    add function update avt

--- public/template/js/main.js
+++ public/template/js/main.js
@@ -417,6 +417,21 @@ function handleRegister(name, email, password) {
     }, 'json');
 }
 
+/*Handle changeimage */
+function handleImageChange(input) {
+    const previewImage = document.getElementById('preview-image');
+
+    if (input.files && input.files[0]) {
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            previewImage.src = e.target.result;
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
 //show cart guest
 function showCartFromLocal() {
     var cart = JSON.parse(localStorage.getItem('guestCart'));
